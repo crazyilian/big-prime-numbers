7b4789778c4b31f690a32b6322cb656265126811
Ilian Andrianov <ilian.and.04@gmail.com>
2025-01-22 12:18:33 +0300

    remove useless include from example

--- example/example.cpp
+++ example/example.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <primality_tests/miller_rabin_prime_test.hpp>
 
-#include "primality_tests/fermat_test.hpp"
-
 using BigPrimeLib::BigInt;
 using BigPrimeLib::PrimalityStatus;
 using BigPrimeLib::miller_rabin_prime_test;
