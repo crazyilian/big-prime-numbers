f271bc4b768b07e88343844aab11e19d691aeb61
Ilian Andrianov <ilian.and.04@gmail.com>
2025-05-10 00:59:30 +0300

    уменьшено количество вложенных scope в PollardRho и PollardP1 (каждый перезапуск алгоритма вынесен в отдельную функцию)

--- src/factorization/pollard_rho_factorization.hpp
+++ src/factorization/pollard_rho_factorization.hpp
@@ -28,28 +28,36 @@ public:
             return 2;
         }
         for (size_t base_i = 0; !restart_times_.has_value() || base_i < restart_times_.value(); ++base_i) {
-            BigInt x = rnd_.uniform(0, n - 1);
-            BigInt y = x;
+            BigInt x0 = rnd_.uniform(0, n - 1);
             BigInt c;
             do {
                 c = rnd_.uniform(1, n - 1);
             } while (c == n - 2);
-            auto f = [&n, &c](const BigInt &x) { return (x * x + c) % n; };
-            for (size_t iter_i = 0; !iter_times_.has_value() || iter_i < iter_times_.value(); ++iter_i) {
-                x = f(x);
-                y = f(f(y));
-                BigInt d = Math::gcd(Math::abs(x - y), n);
-                if (d == n) {
-                    break;
-                } else if (d != 1) {
-                    return d;
-                }
+            if (auto d = run_once(n, c, x0); d.has_value()) {
+                return d;
             }
         }
         return std::nullopt;
     }
 
 private:
+    std::optional<BigInt> run_once(const BigInt &n, const BigInt &c, const BigInt &x0) const {
+        BigInt x = x0;
+        BigInt y = x0;
+        auto f = [&n, &c](const BigInt &x) { return (x * x + c) % n; };
+        for (size_t iter_i = 0; !iter_times_.has_value() || iter_i < iter_times_.value(); ++iter_i) {
+            x = f(x);
+            y = f(f(y));
+            BigInt d = Math::gcd(Math::abs(x - y), n);
+            if (d == n) {
+                break;
+            } else if (d != 1) {
+                return d;
+            }
+        }
+        return std::nullopt;
+    }
+
     std::optional<PrimeTester> prime_tester_;
     Random<RandomGenerator> rnd_;
     std::optional<size_t> restart_times_, iter_times_;
